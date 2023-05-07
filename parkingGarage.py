3252d7aff4e920e4d04a3d0a20ef65e49c595140
Sean Laster <lastersean@yahoo.com>
2023-05-06 22:39:08 -0400

    GroupProject

---
+++ parkingGarage.py
@@ -0,0 +1,16 @@
+# Start Your Code here
+
+class ParkingGargage():
+   
+    def __init__(self):
+        self.tickets = ()
+        self.parkingspaces = ()
+        self.currentticket= {}
+
+    def take_ticket():
+        pass
+    def pay_parking():
+        pass
+    def leave_garage():
+        pass
+
